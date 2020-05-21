eee220776f51f66d59aa7556ac209874563cf999
Amree Zaid <mohd.amree@gmail.com>
2020-05-21 14:36:03 +0800

    Add chatter table to store state

---
+++ app/models/chatter.rb
@@ -0,0 +1,2 @@
+class Chatter < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_21_031702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatters", force: :cascade do |t|
+    t.string "facebook_sender_id"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facebook_sender_id"], name: "index_chatters_on_facebook_sender_id"
+  end
 
 end
