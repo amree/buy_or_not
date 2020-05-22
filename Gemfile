9547e8ebd64f11dcb9a5a53289f3519927165246
Amree Zaid <mohd.amree@gmail.com>
2020-05-23 00:21:21 +0800

    Add simplecov gem to calculate test coverage

--- Gemfile
+++ Gemfile
@@ -62,4 +62,5 @@ end
 group :test do
   gem "vcr"
   gem "webmock"
+  gem "simplecov", require: false
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run "rails generate rspec:install"
+require "simplecov"
+SimpleCov.start
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
@@ -6,8 +9,8 @@ require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
-
 require "sidekiq/testing"
+
 Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
