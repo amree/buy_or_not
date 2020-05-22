52ef1664692b25b13d18818660b9f86a5cb2a5c8
Amree Zaid <mohd.amree@gmail.com>
2020-05-22 23:26:00 +0800

    Add SendMessage Service
    This class will help us send message to a Facebook user anytime we want
    as long as we give the correct ID. This will be used as part of the
    communication between the bot and the user

---
+++ app/services/chats/send_message.rb
@@ -0,0 +1,44 @@
+module Chats
+  class SendMessage
+    def initialize(sender_id:, template:, next_state:)
+      @sender_id = sender_id
+      @template = template
+      @next_state = next_state
+    end
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def call
+      send_message
+      update_state
+    end
+
+    private
+
+    attr_reader(
+      :sender_id,
+      :next_state,
+      :template
+    )
+
+    def send_message
+      access_token = Rails.application.credentials.facebook[:access_token]
+
+      Facebook::Messenger::Bot.deliver({
+        recipient: {id: sender_id},
+        message: template,
+        messaging_type: Facebook::Messenger::Bot::MessagingType::UPDATE
+      }, access_token: access_token)
+    end
+
+    def update_state
+      return if next_state.nil?
+
+      Chatter
+        .find_by(facebook_sender_id: sender_id)
+        .update(state: next_state)
+    end
+  end
+end
---
+++ spec/services/chats/send_message_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe Chats::SendMessage, vcr: true do
+  let(:access_token) { Rails.application.credentials.facebook[:access_token] }
+  let(:chatter) { create(:chatter) }
+  let(:sender_id) { chatter.facebook_sender_id }
+  let(:template) { {text: "Hello"} }
+
+  subject do
+    described_class.call(
+      sender_id: sender_id,
+      template: template,
+      next_state: next_state
+    )
+  end
+
+  before do
+    expect(Bot).to receive(:deliver).with(
+      {
+        recipient: {id: sender_id},
+        message: template,
+        messaging_type: Facebook::Messenger::Bot::MessagingType::UPDATE
+      }, access_token: access_token
+    )
+
+    subject
+  end
+
+  context "with next state" do
+    let(:next_state) { "processing" }
+
+    it { expect(chatter.reload.state).to eql(next_state) }
+  end
+
+  context "without next state" do
+    let(:next_state) { nil }
+
+    it { expect(chatter.reload.state).to eql("initial") }
+  end
+end
